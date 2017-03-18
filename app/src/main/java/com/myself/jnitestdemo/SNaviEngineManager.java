655dd8ba0ea20dfbdc65fc766f62117a1ef8c28b
Jusenr <jusenr@163.com>
2017-03-18 16:18:58 +0800

    add

---
+++ app/src/main/java/com/myself/jnitestdemo/SNaviEngineManager.java
@@ -0,0 +1,16 @@
+package com.myself.jnitestdemo;
+
+/**
+ * Created by T5-Jusenr on 2017/3/18.
+ */
+
+public class SNaviEngineManager {
+
+    public void sendMessage() {
+
+    }
+
+    public static int sendMessage(int msg) {
+        return 0;
+    }
+}
